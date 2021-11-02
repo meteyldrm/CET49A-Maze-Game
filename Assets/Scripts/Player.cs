aee17c648af2c09c373c770d320c85b273be7ae7
Mete Yıldırım <43517475+meteyldrm@users.noreply.github.com>
2021-11-03 01:57:32 +0300

    Add drone for the camera

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -29,12 +29,16 @@ public class Player : MonoBehaviour
     private float _lastAttackTime;
 
     private bool _isInvulnerable;
-    private bool _shrinked = false;
 
     private int _hasJumped = 0;
     
     // Input Checks
     private int _inputDirection = 0;
+
+    private GameObject _drone;
+    private Rigidbody2D _droneRigidbody2D;
+    private bool _isDrone = false;
+    private Vector2 _droneVector = Vector2.zero;
 
 
     private int _playerDirection = -1;
@@ -43,6 +47,8 @@ public class Player : MonoBehaviour
     private void Start()
     {
         _currentHealth = maxHealth;
+        _drone = gameObject.transform.Find("DroneTarget").gameObject;
+        _droneRigidbody2D = _drone.GetComponent<Rigidbody2D>();
     }
 
 
@@ -50,25 +56,7 @@ public class Player : MonoBehaviour
     {
         healthSlider.value = _currentHealth / maxHealth;
         playerAnimator.SetFloat(Speed,Math.Abs(playerRigidbody2D.velocity.x));
-
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            _inputDirection -= 1;
-        }
-        if (Input.GetKeyUp(KeyCode.A))
-        {
-            _inputDirection += 1;
-        }
-
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            _inputDirection += 1;
-        }
-        if (Input.GetKeyUp(KeyCode.D))
-        {
-            _inputDirection -= 1;
-        }
-
+        
         if (Input.GetMouseButton(0) && Time.time > _lastAttackTime + attackRate)
         {
             _lastAttackTime = Time.time;
@@ -77,65 +65,120 @@ public class Player : MonoBehaviour
             aimPoint.z = 0;
             bullet.transform.right = aimPoint - bullet.transform.position;
         }
-
-        if (Input.GetKeyDown(KeyCode.W) && onGround)
-        {
-            playerRigidbody2D.AddForce(Vector2.up * jumpHeight,ForceMode2D.Force);
-            _hasJumped += 1;
-        }
         
-        if (Input.GetKeyDown(KeyCode.W) && !onGround)
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            if (_hasJumped <= 2) {
-                playerRigidbody2D.velocity *= new Vector2(1, 0);
-                playerRigidbody2D.AddForce(Vector2.up * jumpHeight,ForceMode2D.Force);
-                _hasJumped += 1;
+            _inputDirection -= 1;
+            _droneVector.x -= 1;
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            _inputDirection += 1;
+            _droneVector.x += 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            _inputDirection += 1;
+            _droneVector.x += 1;
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            _inputDirection -= 1;
+            _droneVector.x -= 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W)) {
+            _droneVector.y += 1;
+            if (!_isDrone) {
+                if (onGround) {
+                    playerRigidbody2D.AddForce(Vector2.up * jumpHeight,ForceMode2D.Force);
+                    _hasJumped += 1;
+                } else {
+                    if (_hasJumped <= 2) {
+                        playerRigidbody2D.velocity *= new Vector2(1, 0);
+                        playerRigidbody2D.AddForce(Vector2.up * jumpHeight,ForceMode2D.Force);
+                        _hasJumped += 1;
+                    }
+                }
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.S) && !_shrinked)
-        {
-            var transform1 = transform;
-            var localScale = transform1.localScale;
-            localScale = new Vector3(localScale.x,localScale.y / 1.5f,transform1.localPosition.z);
-            transform1.localScale = localScale;
-            _shrinked = true;
+        if (Input.GetKeyUp(KeyCode.W)) {
+            _droneVector.y -= 1;
         }
 
-        if (Input.GetKeyUp(KeyCode.S) && _shrinked)
-        {
-            var transform1 = transform;
-            var localScale = transform1.localScale;
-            localScale = new Vector3(localScale.x,localScale.y * 1.5f,transform1.localPosition.z);
-            transform1.localScale = localScale;
-            _shrinked = false;
+        if (Input.GetKeyDown(KeyCode.S)) {
+            _droneVector.y -= 1;
+            if (!_isDrone) {
+                var transform1 = transform;
+                var localScale = transform1.localScale;
+                localScale = new Vector3(localScale.x,localScale.y / 1.5f,transform1.localPosition.z);
+                transform1.localScale = localScale;
+            }
         }
+
+        if (Input.GetKeyUp(KeyCode.S)) {
+            _droneVector.y += 1;
+            if (!_isDrone) {
+                var transform1 = transform;
+                var localScale = transform1.localScale;
+                localScale = new Vector3(localScale.x,localScale.y * 1.5f,transform1.localPosition.z);
+                transform1.localScale = localScale;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.V)) {
+            _isDrone = true;
+            _droneVector = Vector2.zero;
+            _inputDirection = 0;
+        }
+        
+        if (Input.GetKeyUp(KeyCode.V)) {
+            _isDrone = false;
+            _drone.transform.localPosition = Vector3.zero;
+            _droneVector = Vector2.zero;
+            _inputDirection = 0;
+        }
+        
+        //smoothly reset drone position every frame 
+        if (!_isDrone) _droneRigidbody2D.velocity = playerRigidbody2D.velocity;
     }
 
     private void FixedUpdate()
     {
-        if (onGround)
-        {
-            playerRigidbody2D.AddForce(moveSpeed * _inputDirection * Time.fixedDeltaTime * Vector2.right);
-        }
-        else
-        {
-            playerRigidbody2D.AddForce(moveSpeed * _inputDirection * Time.fixedDeltaTime * Vector2.right / 2);
-        }
-        playerRigidbody2D.velocity *= Vector2.one * 0.95f;
+        if (_isDrone) {
+            //X component has a bounceback for some reason
+            //TODO: Fix it
+            var velocity = _droneRigidbody2D.velocity;
+            velocity.x = Mathf.Lerp(velocity.x, _droneVector.x, 5);
+            velocity.y = Mathf.Lerp(velocity.y, _droneVector.y, 5);
+            velocity  *= 0.75f;
+            _droneRigidbody2D.velocity = velocity;
+        } else {
+            if (onGround)
+            {
+                playerRigidbody2D.AddForce(moveSpeed * _inputDirection * Time.fixedDeltaTime * Vector2.right);
+            }
+            else
+            {
+                playerRigidbody2D.AddForce(moveSpeed * _inputDirection * Time.fixedDeltaTime * Vector2.right / 2);
+            }
+            playerRigidbody2D.velocity *= Vector2.one * 0.95f;
         
         
-        if (playerRigidbody2D.velocity.x > 0.05f && _playerDirection == -1)
-        {
-            transform.Rotate(0,180,0);
-            _playerDirection = 1;
-            healthSlider.direction = Slider.Direction.RightToLeft;
-        }
-        else if (playerRigidbody2D.velocity.x < -0.05f && _playerDirection == 1)
-        {
-            transform.Rotate(0,-180,0);
-            _playerDirection = -1;
-            healthSlider.direction = Slider.Direction.LeftToRight;
+            if (playerRigidbody2D.velocity.x > 0.05f && _playerDirection == -1)
+            {
+                transform.Rotate(0,180,0);
+                _playerDirection = 1;
+                healthSlider.direction = Slider.Direction.RightToLeft;
+            }
+            else if (playerRigidbody2D.velocity.x < -0.05f && _playerDirection == 1)
+            {
+                transform.Rotate(0,-180,0);
+                _playerDirection = -1;
+                healthSlider.direction = Slider.Direction.LeftToRight;
+            }
         }
     }
 
